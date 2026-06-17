16530954810c3ca72f45cc5a9ee7f87d13fe300b
auraecosystem <sandraisom@icloud.com>
2026-06-17 03:38:29 +0100

    Update bash.sh

--- bash.sh
+++ bash.sh
@@ -2,4 +2,6 @@ rpmbuild --define "optflags -O3 -march=native" -ba your_package.spec
 rpm --eval "%{optflags}"
 $ curl https://yangcatalog.org/api/search/vendors
 $ curl https://yangcatalog.org/api/search/catalog
-
+git clone https://github.com/auraecosystem/turbo.git
+cd turbo
+tree -L 2
